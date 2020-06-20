5bf8dab4a8d21570dde3a564e34e9db706217861
Matthias Nott <matthias.nott@sap.com>
2020-06-20 15:42:36 +0100

    Remove Deadlock.
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

--- devonthink/mail2devonthink.sh
+++ devonthink/mail2devonthink.sh
@@ -23,21 +23,23 @@ fi
 
 for src in new cur; do
 	for i in $MAILDIR/$src/*; do
+		if [[ $VERBOSE = true ]]; then
+			echo "Working on file $i";
+		fi
 		FILE=$i;
 		if [[ -f "$FILE" ]]; then
 			TARGET="$(cat $FILE | docker exec --privileged -i $CONTAINER_ID perl /usr/local/bin/subject-decoder.pl)";
+			if [[ $VERBOSE = true ]]; then echo $TARGET; fi
 			if [[ ! -f "$DTINBOX/$TARGET.eml" ]]; then
 				cp -a "$FILE" "$DTINBOX/$TARGET.eml";
 			else
-				while true; do
-					temp_name="${TARGET}.";
-					if [[ ! -f "$DTINBOX/$temp_name.eml" ]]; then
-						if [[ "$temp_name" == "-1" ]]; then break; fi;
-						cp -a "$FILE" "$DTINBOX/$temp_name.eml";
-						TARGET="$temp_name";
-						break;
-					fi
+				while [[ -f "$DTINBOX/${TARGET}.eml" ]]; do
+					TARGET="${TARGET}."
 				done
+				if [[ ! -f "$DTINBOX/$TARGET.eml" ]]; then
+					if [[ $VERBOSE = true ]]; then echo $TARGET; fi
+					cp -a "$FILE" "$DTINBOX/$TARGET.eml";
+				fi
 			fi
 			if [[ -f "$DTINBOX/$TARGET.eml" ]]; then
 				if [[ $VERBOSE == true ]]; then
